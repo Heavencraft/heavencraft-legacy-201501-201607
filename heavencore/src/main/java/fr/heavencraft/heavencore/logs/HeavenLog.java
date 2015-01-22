375f4adb1fecca097f91cc9898c8bd4f3709ec45
LRGN <lrgn@protonmail.com>
2015-01-22 06:53:32 +0100

    [Core] Update core

---
+++ heavencore/src/main/java/fr/heavencraft/heavencore/logs/HeavenLog.java
@@ -0,0 +1,19 @@
+package fr.heavencraft.heavencore.logs;
+
+public abstract class HeavenLog
+{
+	public static HeavenLog getLogger(Class<?> clazz)
+	{
+		return new BukkitHeavenLog(clazz);
+	}
+
+	public abstract void enableDebug();
+
+	public abstract void debug(String format, Object... args);
+
+	public abstract void info(String format, Object... args);
+
+	public abstract void warn(String format, Object... args);
+
+	public abstract void error(String format, Object... args);
+}
