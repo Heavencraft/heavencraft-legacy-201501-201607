32c56f87897237d7c42603f22dc6658c385b2fe3
LRGN <lrgn@protonmail.com>
2015-04-13 20:58:50 +0200

    [Core] Add permissions to access specific worlds

--- heavencore/src/main/java/fr/heavencraft/heavencore/CorePermissions.java
+++ heavencore/src/main/java/fr/heavencraft/heavencore/CorePermissions.java
@@ -11,4 +11,6 @@ public class CorePermissions
 	public static final String TP_COMMAND = "heavencore.commands.tp";
 	public static final String TPHERE_COMMAND = "heavencore.commands.tphere";
 	public static final String TPPOS_COMMAND = "heavencore.commands.tppos";
+
+	public static final String WORLD_ACCESS = "heavencore.world.access.";
 }
