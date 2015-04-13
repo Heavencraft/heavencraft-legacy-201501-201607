591d38a85d6176e9646acd4badc86c138bfa4171
lorgan17 <lorgan17@gmail.com>
2015-04-13 21:02:00 +0200

    [Créa] Things (included world_talent)

--- heavencrea/src/main/java/fr/heavencraft/heavencrea/generator/CreativeChunkGenerator.java
+++ heavencrea/src/main/java/fr/heavencraft/heavencrea/generator/CreativeChunkGenerator.java
@@ -4,10 +4,8 @@ import java.util.Random;
 
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Biome;
-import org.bukkit.generator.ChunkGenerator;
 
-public class CreativeChunkGenerator extends ChunkGenerator
+public class CreativeChunkGenerator extends FlatChunkGenerator
 {
 	public static final int SMALL_PLOT = 2;
 	public static final int MEDIUM_PLOT = 5;
@@ -164,40 +162,5 @@ public class CreativeChunkGenerator extends ChunkGenerator
 			chunk.setBlock(startX - 1, 51, z, Material.AIR);
 			chunk.setBlock(stopX, 51, z, Material.AIR);
 		}
-	}
-
-	/*
-	 * Ground
-	 */
-
-	private static ChunkHolder generateGround(BiomeGrid biomes)
-	{
-		final ChunkHolder chunk = new ChunkHolder();
-
-		for (int x = 0; x != 16; x++)
-		{
-			for (int z = 0; z != 16; z++)
-			{
-				// O -> 4 : BEDROCK
-				for (int y = 0; y != 5; y++)
-					chunk.setBlock(x, y, z, Material.BEDROCK);
-
-				// 5 -> 45 : STONE
-				for (int y = 5; y != 46; y++)
-					chunk.setBlock(x, y, z, Material.STONE);
-
-				// 46 -> 49 : DIRT
-				for (int y = 46; y != 50; y++)
-					chunk.setBlock(x, y, z, Material.DIRT);
-
-				// 50 : GRASS
-				chunk.setBlock(x, 50, z, Material.GRASS);
-
-				// Biome
-				biomes.setBiome(x, z, Biome.PLAINS);
-			}
-		}
-
-		return chunk;
 	}
 }
--- heavencrea/src/main/java/fr/heavencraft/heavencrea/worlds/PortalListener.java
+++ heavencrea/src/main/java/fr/heavencraft/heavencrea/worlds/PortalListener.java
@@ -1,17 +1,19 @@
 package fr.heavencraft.heavencrea.worlds;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPortalEnterEvent;
 
+import fr.heavencraft.heavencore.CorePermissions;
 import fr.heavencraft.heavencore.bukkit.HeavenPlugin;
 import fr.heavencraft.heavencore.bukkit.listeners.AbstractListener;
 
 public class PortalListener extends AbstractListener
 {
+	private static final String TALENT_PERMISSION = CorePermissions.WORLD_ACCESS + WorldsManager.WORLD_TALENT;
+
 	public PortalListener(HeavenPlugin plugin)
 	{
 		super(plugin);
@@ -25,12 +27,31 @@ public class PortalListener extends AbstractListener
 
 		final Block block = event.getLocation().getBlock();
 
-		if (block.getType() != Material.ENDER_PORTAL)
-			return;
-
 		if (block.getWorld() != WorldsManager.getWorldCreative())
 			return;
 
-		plugin.teleportPlayer((Player) event.getEntity(), WorldsManager.getBiomeSpawnLocation());
+		final Player player = (Player) event.getEntity();
+
+		switch (block.getType())
+		{
+			case ENDER_PORTAL:
+				plugin.teleportPlayer(player, WorldsManager.getBiomeSpawnLocation());
+				plugin.sendMessage(player, "Vous avez été téléporté dans le monde {CréaBiome}.");
+				break;
+
+			case PORTAL:
+				if (player.hasPermission(TALENT_PERMISSION))
+					plugin.sendMessage(player, "Vous n'avez pas la permission d'accéder au monde {Talent}.");
+
+				else
+				{
+					plugin.teleportPlayer(player, WorldsManager.getTalentSpawnLocation());
+					plugin.sendMessage(player, "Vous avez été téléporté dans le monde {Talent}.");
+				}
+				break;
+
+			default:
+				break;
+		}
 	}
 }
