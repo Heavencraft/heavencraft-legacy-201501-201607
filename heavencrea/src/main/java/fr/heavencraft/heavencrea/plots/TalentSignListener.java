c8550c93e22f830ef198e6d532283ab19c8a034d
LRGN <lrgn@protonmail.com>
2015-04-19 18:17:07 +0200

    [HeavenCrea] Update

--- heavencrea/src/main/java/fr/heavencraft/heavencrea/plots/TalentSignListener.java
+++ heavencrea/src/main/java/fr/heavencraft/heavencrea/plots/TalentSignListener.java
@@ -1,5 +1,7 @@
 package fr.heavencraft.heavencrea.plots;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.Collection;
 
 import org.bukkit.Bukkit;
@@ -12,19 +14,20 @@ import org.bukkit.event.block.SignChangeEvent;
 import fr.heavencraft.heavencore.exceptions.HeavenException;
 import fr.heavencraft.heavencrea.CreaPermissions;
 import fr.heavencraft.heavencrea.HeavenCrea;
-import fr.heavencraft.heavencrea.users.User;
+import fr.heavencraft.heavenguard.api.Flag;
 import fr.heavencraft.heavenguard.api.Region;
 import fr.heavencraft.heavenguard.api.RegionProvider;
 import fr.heavencraft.heavenguard.bukkit.HeavenGuard;
+import fr.heavencraft.heavenguard.bukkit.RegionUtil;
 
-public class PlotSignListener extends AbstractPlotSignListener
+public class TalentSignListener extends AbstractPlotSignListener
 {
 	private final HeavenCrea plugin;
 	private final HeavenGuard regionPlugin;
 
-	public PlotSignListener(HeavenCrea plugin, HeavenGuard regionPlugin)
+	public TalentSignListener(HeavenCrea plugin, HeavenGuard regionPlugin)
 	{
-		super(plugin, "Parcelle", CreaPermissions.PARCELLE_SIGN);
+		super(plugin, "Talent", CreaPermissions.PARCELLE_SIGN);
 
 		this.plugin = plugin;
 		this.regionPlugin = regionPlugin;
@@ -42,9 +45,9 @@ public class PlotSignListener extends AbstractPlotSignListener
 		if (regions.size() != 0)
 			throw new HeavenException("Une protection existe déjà ici.");
 
-		final Plot plot = getNormalPlotAt(location);
-
-		event.setLine(1, plot.price + " jetons");
+		event.setLine(1, "Parcelle pour");
+		event.setLine(2, "l'admission");
+		event.setLine(3, "en map talent.");
 
 		return true;
 	}
@@ -53,11 +56,7 @@ public class PlotSignListener extends AbstractPlotSignListener
 	protected void onSignClick(Player player, Sign sign) throws HeavenException
 	{
 		// Get the plot
-		final Plot plot = getNormalPlotAt(sign.getLocation());
-
-		// Take the money
-		final User user = plugin.getUserProvider().getUserByUniqueId(player.getUniqueId());
-		user.updateBalance(-plot.price);
+		final Plot plot = getTalentPlotAt(sign.getLocation());
 
 		// Create the region
 		final Region region = createRegion(player, sign.getWorld().getName(), plot);
@@ -65,37 +64,39 @@ public class PlotSignListener extends AbstractPlotSignListener
 		// Set redstone border
 		for (int x = plot.minX; x <= plot.maxX; x++)
 		{
-			Bukkit.getWorld("world_creative").getBlockAt(x, 51, plot.minZ).setType(Material.REDSTONE_BLOCK);
-			Bukkit.getWorld("world_creative").getBlockAt(x, 51, plot.maxZ).setType(Material.REDSTONE_BLOCK);
+			Bukkit.getWorld("world_creative").getBlockAt(x, 51, plot.minZ).setType(Material.LAPIS_BLOCK);
+			Bukkit.getWorld("world_creative").getBlockAt(x, 51, plot.maxZ).setType(Material.LAPIS_BLOCK);
 		}
 		for (int z = plot.minZ; z <= plot.maxZ; z++)
 		{
-			Bukkit.getWorld("world_creative").getBlockAt(plot.minX, 51, z).setType(Material.REDSTONE_BLOCK);
-			Bukkit.getWorld("world_creative").getBlockAt(plot.maxX, 51, z).setType(Material.REDSTONE_BLOCK);
+			Bukkit.getWorld("world_creative").getBlockAt(plot.minX, 51, z).setType(Material.LAPIS_BLOCK);
+			Bukkit.getWorld("world_creative").getBlockAt(plot.maxX, 51, z).setType(Material.LAPIS_BLOCK);
 		}
 
 		sign.getBlock().breakNaturally();
-		plugin.sendMessage(player, "Vous venez d'acheter la parcelle {%1$s} pour {%2$s} jetons.",
+		plugin.sendMessage(player,
+				"Vous venez de recevoir la parcelle {%1$s}. Celle-ci sera supprimée dans 3 semaines.",
 				region.getName(), plot.price);
 	}
 
 	private Region createRegion(Player player, String world, final Plot plot) throws HeavenException
 	{
 		final RegionProvider regionProvider = regionPlugin.getRegionProvider();
-		String regionName;
-		int i = 1;
+		final String regionName = "talent_" + player.getName();
 
-		do
-		{
-			regionName = "parcelle_" + player.getName() + "_" + i++;
-		}
-		while (regionProvider.regionExists(regionName));
+		if (regionProvider.regionExists(regionName))
+			throw new HeavenException("Vous avez déjà une parcelle Talent.");
 
 		final Region region = regionProvider.createRegion(regionName, world,//
 				plot.minX, 0, plot.minZ, //
 				plot.maxX, 0xFF, plot.maxZ);
 
 		region.addMember(player.getUniqueId(), true);
+		RegionUtil.saveState(region);
+
+		final Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_MONTH, 21);
+		region.getFlagHandler().setTimestampFlag(Flag.REMOVE_TIMESTAMP, new Timestamp(cal.getTimeInMillis()));
 		return region;
 	}
 
