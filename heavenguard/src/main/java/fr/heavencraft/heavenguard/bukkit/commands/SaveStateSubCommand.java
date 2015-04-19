d61c821af0d81f10b41b018a2dd07e6408ea870f
lorgan17 <lorgan17@gmail.com>
2015-04-19 18:16:38 +0200

    [HeavenGuard] Update

--- heavenguard/src/main/java/fr/heavencraft/heavenguard/bukkit/commands/SaveStateSubCommand.java
+++ heavenguard/src/main/java/fr/heavencraft/heavenguard/bukkit/commands/SaveStateSubCommand.java
@@ -1,55 +1,33 @@
 package fr.heavencraft.heavenguard.bukkit.commands;
 
-import java.io.IOException;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 
-import com.sk89q.worldedit.WorldEditException;
-
 import fr.heavencraft.heavencore.exceptions.HeavenException;
-import fr.heavencraft.heavenguard.api.Flag;
 import fr.heavencraft.heavenguard.api.HeavenGuardPermissions;
 import fr.heavencraft.heavenguard.api.Region;
 import fr.heavencraft.heavenguard.bukkit.HeavenGuard;
-import fr.heavencraft.heavenguard.bukkit.WorldEditUtil;
+import fr.heavencraft.heavenguard.bukkit.RegionUtil;
 
-public class SaveStateSubCommand extends AbstractSubCommand
+public class SavestateSubCommand extends AbstractSubCommand
 {
-	public SaveStateSubCommand(HeavenGuard plugin)
+	public SavestateSubCommand(HeavenGuard plugin)
 	{
-		super(plugin, HeavenGuardPermissions.INFO_COMMAND);
+		super(plugin, HeavenGuardPermissions.SAVESTATE_COMMAND);
 	}
 
 	@Override
 	public void execute(CommandSender sender, String regionName, String[] args) throws HeavenException
 	{
 		final Region region = plugin.getRegionProvider().getRegionByName(regionName);
-		final World world = Bukkit.getWorld(region.getWorld());
-		final Location pos1 = new Location(world, region.getMinX(), region.getMinY(), region.getMinZ());
-		final Location pos2 = new Location(world, region.getMaxX(), region.getMaxY(), region.getMaxZ());
 
-		try
-		{
-			final byte[] schematic = WorldEditUtil.save(world, pos1, pos2);
-
-			region.getFlagHandler().setByteArrayFlag(Flag.STATE, schematic);
-			plugin.sendMessage(sender, "Done");
-		}
-		catch (WorldEditException | IOException e)
-		{
-			e.printStackTrace();
-			plugin.sendMessage(sender, "Fail");
-		}
+		plugin.sendMessage(sender, "Sauvegarde de la protection {%1$s}...", region.getName());
+		RegionUtil.loadState(region);
+		plugin.sendMessage(sender, "La protection {%1$s} a bien été sauvegardée.", region.getName());
 	}
 
 	@Override
 	public void sendUsage(CommandSender sender)
 	{
-		// TODO Auto-generated method stub
-
+		plugin.sendMessage(sender, "/rg {savestate} <protection>");
 	}
-
 }
