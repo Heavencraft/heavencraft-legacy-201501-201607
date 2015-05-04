ab6f333dd52893945de690d2799a9b7de44decc9
lorgan17 <lorgan17@gmail.com>
2015-05-04 23:20:10 +0200

    [Crea] Permission to not have color for admins.

--- heavencrea/src/main/java/fr/heavencraft/heavencrea/CreaPermissions.java
+++ heavencrea/src/main/java/fr/heavencraft/heavencrea/CreaPermissions.java
@@ -12,6 +12,8 @@ public class CreaPermissions
 	public static final String PLOT_COMMAND = "heavencrea.commands.plot";
 	public static final String TPHOME_COMMAND = "heavencrea.commands.tphome";
 
+	public static final String HIDE_TABCOLOR = "heavencrea.tabcolor.hide";
+
 	public static final String TALENT = CorePermissions.WORLD_ACCESS + WorldsManager.WORLD_TALENT;
-	public static final String ARCHITECT = CorePermissions.WORLD_ACCESS + WorldsManager.WORLD_TALENT;
+	public static final String ARCHITECT = CorePermissions.WORLD_ACCESS + WorldsManager.WORLD_ARCHITECT;
 }
