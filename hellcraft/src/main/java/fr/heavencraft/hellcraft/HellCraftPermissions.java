1f5a0e7856aae2e9bf9626c2380367032ae4b512
lorgan17 <lorgan17@gmail.com>
2015-07-12 22:37:35 +0200

    [HellCraft] Remove useless code an rename perms

--- hellcraft/src/main/java/fr/heavencraft/hellcraft/HellCraftPermissions.java
+++ hellcraft/src/main/java/fr/heavencraft/hellcraft/HellCraftPermissions.java
@@ -2,7 +2,6 @@ package fr.heavencraft.hellcraft;
 
 public class HellCraftPermissions
 {
-	public static final String BACK_COMMAND = "hellcraft.back";
-	public static final String PTIME_COMMAND = "hellcraft.ptime";
-	public static final String SPAWN_COMMAND = "hellcraft.spawn";
+	public static final String PTIME_COMMAND = "hellcraft.commands.ptime";
+	public static final String SPAWN_COMMAND = "hellcraft.commands.spawn";
 }
