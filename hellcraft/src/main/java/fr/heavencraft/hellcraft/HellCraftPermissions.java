ac5c168451db8dcacb62e61211b82088c7f0f1c5
lorgan17 <lorgan17@gmail.com>
2015-07-12 21:13:46 +0200

    [HellCraft] /spawn command must be in VIP.

--- hellcraft/src/main/java/fr/heavencraft/hellcraft/HellCraftPermissions.java
+++ hellcraft/src/main/java/fr/heavencraft/hellcraft/HellCraftPermissions.java
@@ -4,4 +4,5 @@ public class HellCraftPermissions
 {
 	public static final String BACK_COMMAND = "hellcraft.back";
 	public static final String PTIME_COMMAND = "hellcraft.ptime";
+	public static final String SPAWN_COMMAND = "hellcraft.spawn";
 }
