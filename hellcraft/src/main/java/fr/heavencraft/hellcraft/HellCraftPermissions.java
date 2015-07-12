1bc52693fd99f1cd7900601789b656ab2c14b81b
Manu67100 <manuel@bsbco.de>
2015-07-13 00:06:26 +0200

    [Hellcraft] Small tweaks.

--- hellcraft/src/main/java/fr/heavencraft/hellcraft/HellCraftPermissions.java
+++ hellcraft/src/main/java/fr/heavencraft/hellcraft/HellCraftPermissions.java
@@ -4,4 +4,5 @@ public class HellCraftPermissions
 {
 	public static final String PTIME_COMMAND = "hellcraft.commands.ptime";
 	public static final String SPAWN_COMMAND = "hellcraft.commands.spawn";
+	public static final String VIP_GRADE = "hellcraft.grade.vip";
 }
