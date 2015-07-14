9fc501c56528773b4d0b9e64fad213c832cbf547
Manuel R <rauscher.manuel@gmail.com>
2015-07-14 13:24:58 +0200

    [Hellcraft] Better way to check first connection.

--- hellcraft/src/main/java/fr/heavencraft/hellcraft/Utils.java
+++ hellcraft/src/main/java/fr/heavencraft/hellcraft/Utils.java
@@ -69,13 +69,4 @@ public class Utils
 		}
 		return new Location(world, x + 0.5D, y, z + 0.5D, loc.getYaw(), loc.getPitch());
 	}
-	
-	public static boolean isInventoryEmpty(Player player)
-	{
-		ItemStack[] contents = player.getInventory().getContents();
-		for (ItemStack content : contents)
-			if (content != null)
-				return false;
-		return true;
-	}
 }
