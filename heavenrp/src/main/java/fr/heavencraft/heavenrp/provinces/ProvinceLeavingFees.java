538d9692b87ef60bcd05b368fe1bb3bc48f12c5f
Manuel R <rauscher.manuel@gmail.com>
2015-11-12 17:16:12 +0100

    [RP] Recurrent Province switching rises leaving fees.
    + Added incremental counter for provinces changes.
    + Added rising fees when leaving province.

---
+++ heavenrp/src/main/java/fr/heavencraft/heavenrp/provinces/ProvinceLeavingFees.java
@@ -0,0 +1,40 @@
+package fr.heavencraft.heavenrp.provinces;
+
+import fr.heavencraft.heavenrp.database.users.User;
+
+public class ProvinceLeavingFees
+{
+	/**
+	 * Returns the fees the player has to pay if he leaves a province
+	 * @param user
+	 * @return fees (positive value)
+	 */
+	public static int getLeavingFees(User user)
+	{
+		switch (user.getProvinceChanges())
+		{
+			case 0:
+			case 1:
+				return 50;
+			case 2:
+				return 250;
+			case 3:
+				return 740;
+			case 4:
+				return 1250;
+			case 5:
+				return 1700;
+			case 6:
+				return 2400;
+			case 7:
+				return 3000;
+			case 8:
+				return 3600;
+			case 9:
+				return 4200;
+			
+			default:
+				return 5000;
+		}
+	}
+}
