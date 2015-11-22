f7c3fb2b20f62f74833f2df41dfa1e98a074423b
Manu67100 <manuel@bsbco.de>
2015-11-22 23:38:21 +0100

    [RP] Starting implementation of dungeons.
    + Dungeons stored into memory

---
+++ heavenrp/src/main/java/fr/heavencraft/heavenrp/dungeon/DungeonListener.java
@@ -0,0 +1,13 @@
+package fr.heavencraft.heavenrp.dungeon;
+
+import fr.heavencraft.heavencore.bukkit.HeavenPlugin;
+import fr.heavencraft.heavencore.bukkit.listeners.AbstractListener;
+
+public class DungeonListener extends AbstractListener<HeavenPlugin>
+{
+	public DungeonListener(HeavenPlugin plugin)
+	{
+		super(plugin);
+	}
+
+}
