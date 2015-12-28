412a8db8a00cec11c53a8e8612892f354feb900e
Manu67100 <manuel@bsbco.de>
2015-12-28 17:21:12 +0100

    [VIP] Particle Menu

--- heavenvip/src/main/java/fr/heavencraft/heavenvip/MenuProvider.java
+++ heavenvip/src/main/java/fr/heavencraft/heavenvip/MenuProvider.java
@@ -1,5 +1,12 @@
 package fr.heavencraft.heavenvip;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -32,7 +39,69 @@ public class MenuProvider
 	}
 	
 	public Menu getParticleMenu(Player p) {
-		return null;
+		
+		Menu menu = new Menu("§7Particules -- HPS", 2);
+		// Get the list of particles
+		try (PreparedStatement ps = HeavenVIP.getMainConnection()
+				.getConnection().prepareStatement(
+						"SELECT vip_pack.*, CASE WHEN EXISTS(" +
+						"SELECT 1 FROM vip_bought WHERE vip_bought.user_id = '19034' AND vip_bought.pack_id = vip_pack.vip_pack_id LIMIT 1)" +
+						"THEN 1 " +
+						"ELSE 0 " +
+						"END as owns " +
+						"FROM vip_pack WHERE vip_pack.effect_type = 'p'"))
+		{
+			final ResultSet rs = ps.executeQuery();
+			int x = 0;
+			int y = 0;
+			// For each pack
+			while (rs.next())
+			{
+				// Generate representative item
+				// Wool durability: grey:7 green:5 other:14
+				Material material = Material.WOOL;
+				String displayName = ChatColor.translateAlternateColorCodes('§', rs.getString("name"));
+				boolean owns = rs.getBoolean("owns");
+				short durability = (short) 14;
+				
+				Date date = new Date(System.currentTimeMillis());
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(date);
+				int actualMonth = cal.get(Calendar.MONTH) + 1;
+				int packMonth = rs.getInt("month");
+				
+				if(owns)
+					durability = (short)5;
+				else if(packMonth == actualMonth)
+					durability = (short)14;
+				else
+					durability = (short)7;
+				
+				// Create menu entry
+				menu.addOption(x, y, new Option(material, durability, displayName) {
+					@Override
+					public void onClick(Menu menu, Player player, ItemStack cursor, ItemStack current, ClickType type) throws HeavenException
+					{
+						
+					}
+				});
+				
+				// Handle warping
+				x++;
+				// have we filled a line?
+				if(x >= 9)
+				{
+					x = 0;
+					y++;
+				}		
+			}
+		}
+		catch (final SQLException ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return menu;
 	}
 
 }
