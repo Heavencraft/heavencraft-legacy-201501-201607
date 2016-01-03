ba48ff40e892031ac478e601a5c809ca72a3b17a
Manuel R <rauscher.manuel@gmail.com>
2016-01-03 12:30:22 +0100

    [VIP] Basic shop inventory and backend interaction.

---
+++ heavenvip/src/main/java/fr/heavencraft/heavenvip/UpdateHPSBalanceQuery.java
@@ -0,0 +1,35 @@
+package fr.heavencraft.heavenvip;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import fr.heavencraft.async.queries.AbstractQuery;
+import fr.heavencraft.heavencore.exceptions.HeavenException;
+
+public class UpdateHPSBalanceQuery extends AbstractQuery
+{
+	private static final String QUERY = "UPDATE heavencraft_users SET balance = balance + ? WHERE username = ? AND balance + ? >= 0 LIMIT 1";
+
+	private final String name;
+	private final int delta;
+
+	public UpdateHPSBalanceQuery(String name, int delta)
+	{
+		this.name = name;
+		this.delta = delta;
+	}
+
+	@Override
+	public void executeQuery() throws HeavenException, SQLException
+	{
+		try (PreparedStatement ps = HeavenVIP.getMainConnection().getConnection().prepareStatement(QUERY))
+		{
+			ps.setInt(1, delta);
+			ps.setString(2, name);
+			ps.setInt(3, delta);
+
+			if (ps.executeUpdate() == 0)
+				throw new HeavenException("Vous n'avez pas assez d'argent sur votre compte.");
+		}
+	}
+}
