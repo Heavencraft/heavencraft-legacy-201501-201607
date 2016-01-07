2b0fb9d2f8b5484eaf75ec3a8e4924357f63ad55
Manuel R <manuel@bsbco.de>
2016-01-07 23:56:51 +0100

    [VIP] New menu architecture. Still not proper refreshing of menus.

---
+++ heavenvip/src/main/java/fr/heavencraft/heavenvip/menus/ParticlePackMenu.java
@@ -0,0 +1,114 @@
+package fr.heavencraft.heavenvip.menus;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import fr.heavencraft.heavencore.exceptions.HeavenException;
+import fr.heavencraft.heavencore.utils.chat.ChatUtil;
+import fr.heavencraft.heavencore.utils.menu.ClickType;
+import fr.heavencraft.heavencore.utils.menu.Menu;
+import fr.heavencraft.heavencore.utils.menu.options.Option;
+import fr.heavencraft.heavenvip.HpsManager;
+import fr.heavencraft.heavenvip.MenuUtils;
+import fr.heavencraft.heavenvip.vippacks.PackType;
+import fr.heavencraft.heavenvip.vippacks.VipPack;
+import fr.heavencraft.heavenvip.vippacks.VipPackProvider;
+
+public class ParticlePackMenu extends VipMenu
+{
+	private final static String ITEM_NOT_FOR_SALE = "Cet objet n'est pas a vendre en ce moment.";
+	private final static String NOT_ENOUGH_HPS = "Vous n'avez pas assez d'HPS, plus d'informations: www.heavencraft.fr";
+
+	public ParticlePackMenu(Player p)
+	{
+		super(new Menu("Packs Particles", 4));
+		// Variables for item coordinates inside inventory
+		int x = 0;
+		int y = 0;
+		for(final VipPack pack : VipPackProvider.getPacks(p, PackType.PARTICLE)) {
+			// Can we buy this item? Do we own it? -> set durability for item color.
+			Date date = new Date(System.currentTimeMillis());
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(date);
+			int actualMonth = cal.get(Calendar.MONTH) + 1;
+			short packStateColor;
+			if(pack.isOwned())
+				packStateColor = (short)5;
+			else if(pack.getMonthOfAvailability() == actualMonth || pack.getMonthOfAvailability() <= 0)
+				packStateColor = (short)14;
+			else
+				packStateColor = (short)7;
+			final short itemColor = packStateColor;
+
+			super.m.addOption(x, y, new Option(Material.WOOL, itemColor, pack.getPackName(), 
+					MenuUtils.generateShopItemLore(pack.getPackName(), pack.getPrice(), pack.getDescription())) {
+				@Override
+				public void onClick(Menu menu, Player player, ItemStack cursor, ItemStack current,
+						ClickType type) throws HeavenException
+				{
+					if(itemColor == 5) {
+						// We own this pack, open equip menu.
+						ChatUtil.broadcastMessage("OPEN EQUIP MENU");
+						//TODO open equip menu
+					}
+					else if(itemColor == 7) {
+						// Item is not for sale this month.
+						ChatUtil.sendMessage(player, ITEM_NOT_FOR_SALE);
+					}
+					else if(itemColor == 14) {
+						// Item is for sale, check if player has enough to buy.
+						int hpsCount = HpsManager.getBalance(p.getName());
+						if(pack.getPrice() > hpsCount){
+							// Player is too poor
+							ChatUtil.sendMessage(player, NOT_ENOUGH_HPS);
+							return;
+						}
+						// We can buy it
+						ChatUtil.broadcastMessage("OPEN ITEM BUY PROMPT");
+						//TODO open Item buy prompt
+					}
+				}
+			});
+			// Update coordinates			
+			if(++x >= 9) {
+				x = 0;
+				++y;
+			}
+		}
+	}
+
+	@Override
+	public void openMenu(Player p)
+	{
+		// Attach Navigation Bar
+		MenuUtils.attachNavigationBar(this, new MainMenu(), p, super.m.getHeight());
+		try
+		{
+			super.OpenMenu(p);
+		}
+		catch (HeavenException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void openNewMenu(Player p)
+	{
+		// Attach Navigation Bar
+		MenuUtils.attachNavigationBar(this, new MainMenu(), p, super.m.getHeight());
+		try
+		{
+			super.OpenNewMenu(p);
+		}
+		catch (HeavenException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
