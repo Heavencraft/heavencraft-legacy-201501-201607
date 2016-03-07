17b701b98775040289d9586321531d685dc0fe6c
Manu67100 <manuel@bsbco.de>
2016-03-07 01:09:26 +0100

    [RP] Dungeon Draft

--- heavenrp/src/main/java/fr/heavencraft/heavenrp/dungeon/DungeonUtils.java
+++ heavenrp/src/main/java/fr/heavencraft/heavenrp/dungeon/DungeonUtils.java
@@ -2,8 +2,22 @@ package fr.heavencraft.heavenrp.dungeon;
 
 import org.bukkit.entity.Player;
 
+import fr.heavencraft.heavencore.utils.chat.ChatUtil;
+
 public class DungeonUtils
 {
-
-	
+	/**
+	 * Displays a list of available dungeons and their state
+	 * @param p
+	 */
+	public static void ListDungeons(final Player p) {
+		ChatUtil.sendMessage(p, "###### Donjons ######");
+		int i = 1; // Counter
+		for(DungeonManager.Dungeon dg : DungeonManager.Dungeons.values()){
+			char linePrefix = (i >= DungeonManager.Dungeons.values().size()) ? '└' : '├';
+			ChatUtil.sendMessage(p, "%1$c ({%2$d}) %3$s | Joueurs: {%4$d}/{%5$d}",
+					linePrefix, dg.getId(), dg.getName(), dg.PlayerCount, dg.getRequiredPlayer());
+			++i;
+		}
+	}
 }
