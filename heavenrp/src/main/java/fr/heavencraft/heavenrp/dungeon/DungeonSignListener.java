324b3084ec42d98260c541deb343ac844db404ff
Manuel R <rauscher.manuel@gmail.com>
2016-03-10 03:52:02 +0100

    [RP] Dungeons commands, events fixes...

--- heavenrp/src/main/java/fr/heavencraft/heavenrp/dungeon/DungeonSignListener.java
+++ heavenrp/src/main/java/fr/heavencraft/heavenrp/dungeon/DungeonSignListener.java
@@ -15,29 +15,36 @@ import fr.heavencraft.heavenrp.RPPermissions;
 public class DungeonSignListener extends AbstractSignListener
 {
 	private static final String NOT_ENOUGH_ARGUMENTS = "Il manque des arguments a ce panneau.";
-	
+	private static final String WRONG_OPERATOR = "L'opérateur est érroné.";
+
 	public DungeonSignListener(HeavenRP plugin)
 	{
 		super(plugin, "Donjon", RPPermissions.DUNGEON_ADMIN);
 	}
-	
-	public enum DungeonSignType {
+
+	public enum DungeonSignType
+	{
 		JOIN("Join", ChatColor.BLUE + "Entrer"),
 		NEXT("Next", ChatColor.BLUE + "Continuer"),
-		LEAVE("Quitter", ChatColor.BLUE + "Quitter");
-		
+		LEAVE("Leave", ChatColor.BLUE + "Quitter"),
+		END("End", ChatColor.BLUE + "Sortie");
+
 		private String signText = "";
 		private String signDisplayText = "";
-		
-		DungeonSignType(String text, String displayed){
+
+		DungeonSignType(String text, String displayed)
+		{
 			this.signText = text;
 			this.signDisplayText = displayed;
 		}
-		
-		public String toString(){
+
+		public String toString()
+		{
 			return this.signText;
 		}
-		public String getDisplay(){
+
+		public String getDisplay()
+		{
 			return this.signDisplayText;
 		}
 	}
@@ -45,43 +52,52 @@ public class DungeonSignListener extends AbstractSignListener
 	@Override
 	protected boolean onSignPlace(Player player, SignChangeEvent event) throws HeavenException
 	{
-		final String line = event.getLine(1);
-		
-		// ## Signs without arguments	
-		if(line.equalsIgnoreCase(DungeonSignType.LEAVE.toString())) {
+		final String operand = event.getLine(1);
+
+		// ## Signs without arguments
+		if (operand.equalsIgnoreCase(DungeonSignType.LEAVE.toString()))
+		{
 			event.setLine(1, DungeonSignType.LEAVE.getDisplay());
 			return true;
 		}
-		
+
+		if (operand.equalsIgnoreCase(DungeonSignType.END.toString()))
+		{
+			event.setLine(1, DungeonSignType.END.getDisplay());
+			return true;
+		}
+
 		// ## Signs with argument
 		final String argument = event.getLine(2);
 		// Has an argument been passed?
-		if(argument == null || argument.length() <= 0){
+		if (argument == null || argument.length() <= 0)
+		{
 			ChatUtil.sendMessage(player, NOT_ENOUGH_ARGUMENTS);
 			return false;
 		}
-		
+
 		// Join sign
-		/* [Donjon]
-		 * JOIN
-		 * <dungeonName>
+		/*
+		 * [Donjon] JOIN <dungeonName>
 		 */
-		if(line.equalsIgnoreCase(DungeonSignType.JOIN.toString())) {
+		if (operand.equalsIgnoreCase(DungeonSignType.JOIN.toString()))
+		{
 			event.setLine(1, DungeonSignType.JOIN.getDisplay());
 			return true;
 		}
 		// Next sign
-		/* [Donjon]
-		 * NEXT
-		 * <dungeonRoomId>
+		/*
+		 * [Donjon] NEXT <dungeonRoomId>
 		 */
-		if(line.equalsIgnoreCase(DungeonSignType.NEXT.toString())) {
-			if(DevUtil.toUint(argument) >= 0){
+		if (operand.equalsIgnoreCase(DungeonSignType.NEXT.toString()))
+		{
+			if (DevUtil.toUint(argument) >= 0)
+			{
 				event.setLine(1, DungeonSignType.NEXT.getDisplay());
 				return true;
 			}
 		}
-		
+		ChatUtil.sendMessage(player, WRONG_OPERATOR);
 		return false;
 	}
 
@@ -89,17 +105,25 @@ public class DungeonSignListener extends AbstractSignListener
 	protected void onSignClick(Player player, Sign sign) throws HeavenException
 	{
 		final String line = sign.getLine(1);
-		
-		if(line.equals(DungeonSignType.NEXT.getDisplay())) {
+
+		if (line.equals(DungeonSignType.NEXT.getDisplay()))
+		{
 			DungeonManager.AttempNextRoom(player, DevUtil.toUint(sign.getLine(2)));
 			return;
 		}
-		if(line.equals(DungeonSignType.JOIN.getDisplay())) {
-			DungeonManager.PlayerJoin(player, null);
+		if (line.equals(DungeonSignType.JOIN.getDisplay()))
+		{
+			DungeonManager.PlayerJoin(player, sign.getLine(2));
 			return;
 		}
-		if(line.equals(DungeonSignType.LEAVE.getDisplay())) {
+		if (line.equals(DungeonSignType.LEAVE.getDisplay()))
+		{
 			DungeonManager.PlayerLeave(player);
+			return;
+		}
+		if (line.equals(DungeonSignType.END.getDisplay()))
+		{
+			DungeonManager.AttempEndDungeon(player);
 			return;
 		}
 	}
