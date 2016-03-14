10e4e309010152274a2dcdb239ca8f9f0a10628b
Manuel R <rauscher.manuel@gmail.com>
2016-03-14 17:00:07 +0100

    [VIP] Packs with id > 12 are now included in menu

--- heavenvip/src/main/java/fr/heavencraft/heavenvip/movments/MovmentParticlePackMenu.java
+++ heavenvip/src/main/java/fr/heavencraft/heavenvip/movments/MovmentParticlePackMenu.java
@@ -28,59 +28,69 @@ public class MovmentParticlePackMenu extends VipMenu
 	private final static String ITEM_NOT_FOR_SALE = "Cet objet n'est pas a vendre en ce moment.";
 	private final static String NOT_ENOUGH_HPS = "Vous n'avez pas assez d'HPS, plus d'informations: www.heavencraft.fr";
 	private final static String ITEM_BOUGHT = "Merci pour votre achat! Vous pouvez désormez vous en équiper.";
-	
+
 	public MovmentParticlePackMenu(Player p)
 	{
 		super(new Menu("Packs Particles", 4));
 		// Variables for item coordinates inside inventory
 		int x = 0;
 		int y = 0;
-		for(final VipPack pack : VipPackProvider.getPacks(p, PackType.PARTICLE)) {
-			// Can we buy this item? Do we own it? -> set durability for item color.
+		for (final VipPack pack : VipPackProvider.getPacks(p, PackType.PARTICLE))
+		{
+			// Can we buy this item? Do we own it? -> set durability for item
+			// color.
 			Date date = new Date(System.currentTimeMillis());
 			Calendar cal = Calendar.getInstance();
 			cal.setTime(date);
 			int actualMonth = cal.get(Calendar.MONTH) + 1;
 			short packStateColor;
-			if(pack.isOwned())
-				packStateColor = (short)5;
-			else if(pack.getMonthOfAvailability() == actualMonth || pack.getMonthOfAvailability() <= 0)
-				packStateColor = (short)14;
+			if (pack.isOwned())
+				packStateColor = (short) 5;
+			else if (pack.getMonthOfAvailability() == actualMonth || pack.getMonthOfAvailability() <= 0
+					|| pack.getMonthOfAvailability() > 12)
+				packStateColor = (short) 14;
 			else
-				packStateColor = (short)7;
+				packStateColor = (short) 7;
 			final short itemColor = packStateColor;
 
-			super.m.addOption(x, y, new Option(Material.WOOL, itemColor, pack.getPackName(), 
-					MenuUtils.generateShopItemLore(pack.getPackName(), pack.getPrice(), pack.getDescription())) {
+			super.m.addOption(x, y, new Option(Material.WOOL, itemColor, pack.getPackName(),
+					MenuUtils.generateShopItemLore(pack.getPackName(), pack.getPrice(), pack.getDescription()))
+			{
 				@Override
-				public void onClick(Menu menu, Player player, ItemStack cursor, ItemStack current,
-						ClickType type) throws HeavenException
+				public void onClick(Menu menu, Player player, ItemStack cursor, ItemStack current, ClickType type)
+						throws HeavenException
 				{
-					if(itemColor == 5) {
+					if (itemColor == 5)
+					{
 						// We own this pack, open equip menu.
 						EquipMenu eMenu = new EquipMenu(player, pack.getPackName(), pack.getVipPackId());
 						eMenu.openMenu(player);
 					}
-					else if(itemColor == 7) {
+					else if (itemColor == 7)
+					{
 						// Item is not for sale this month.
 						ChatUtil.sendMessage(player, ITEM_NOT_FOR_SALE);
 					}
-					else if(itemColor == 14) {
+					else if (itemColor == 14)
+					{
 						// Item is for sale, check if player has enough to buy.
 						int hpsCount = HpsManager.getBalance(p.getName());
-						if(pack.getPrice() > hpsCount){
+						if (pack.getPrice() > hpsCount)
+						{
 							// Player is too poor
 							ChatUtil.sendMessage(player, NOT_ENOUGH_HPS);
 							return;
 						}
 						// Update balance
-						QueriesHandler.addQuery(new UpdateHPSBalanceQuery(player.getName(), -pack.getPrice()) {
+						QueriesHandler.addQuery(new UpdateHPSBalanceQuery(player.getName(), -pack.getPrice())
+						{
 							@Override
-							public void onSuccess() {
+							public void onSuccess()
+							{
 								// Add pack
 								VipPackProvider.addPack(player, pack.getVipPackId());
 								ChatUtil.sendMessage(player, ITEM_BOUGHT);
-								//TODO open equip menu
+								// TODO open equip menu
 								try
 								{
 									MenuAPI.openMenu(player, new MovmentParticlePackMenu(player).GetMenu());
@@ -90,8 +100,10 @@ public class MovmentParticlePackMenu extends VipMenu
 									e.printStackTrace();
 								}
 							}
+
 							@Override
-							public void onHeavenException(HeavenException ex) {
+							public void onHeavenException(HeavenException ex)
+							{
 								// Error
 								ChatUtil.sendMessage(player, ex.getMessage());
 							}
@@ -99,8 +111,9 @@ public class MovmentParticlePackMenu extends VipMenu
 					}
 				}
 			});
-			// Update coordinates			
-			if(++x >= 9) {
+			// Update coordinates
+			if (++x >= 9)
+			{
 				x = 0;
 				++y;
 			}
