bcfc545d9f50dfc4378d652e20c2d96829e86ba6
Balthazar Frolin <balthazarfrolin@gmail.com>
2016-04-03 18:44:10 +0200

    [Heaven Event] add HeavenEvent plugin

---
+++ heavenevent/src/main/java/fr/heavencraft/heavenevent/HeavenEvent.java
@@ -0,0 +1,13 @@
+package fr.heavencraft.heavenevent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HeavenEvent extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+}
