6000caebb698ac53985ad2a46a4fe769f99388b8
Manuel R <rauscher.manuel@gmail.com>
2016-04-05 15:59:58 +0200

    [VIP] Various changes + New test for colored effects and notes + Fixed speed ignored when 4 params given + Clarified separator for 4th param in documentation

--- heavenvip/src/main/java/fr/heavencraft/heavenvip/movments/AppliedDescriptorProperties.java
+++ heavenvip/src/main/java/fr/heavencraft/heavenvip/movments/AppliedDescriptorProperties.java
@@ -2,47 +2,55 @@ package fr.heavencraft.heavenvip.movments;
 
 import fr.heavencraft.heavencore.utils.particles.ParticleEffect;
 
-public class AppliedDescriptorProperties {
+public class AppliedDescriptorProperties
+{
 	private final ParticleEffect effect;
 	private final int effectAmount;
 	private final float speed;
 	private final ParticleEffect.OrdinaryColor oc;
 	private final ParticleEffect.NoteColor nc;
-	
+
 	/**
 	 * 
-	 * @param effect What kind of effect
-	 * @param effectAmount How many particles
+	 * @param effect
+	 *            What kind of effect
+	 * @param effectAmount
+	 *            How many particles
 	 */
-	public AppliedDescriptorProperties(ParticleEffect effect, int effectAmount, float speed) {
+	public AppliedDescriptorProperties(ParticleEffect effect, int effectAmount, float speed)
+	{
 		this.effect = effect;
 		this.effectAmount = effectAmount;
 		this.speed = speed;
 		this.oc = null;
 		this.nc = null;
 	}
-	
-	public AppliedDescriptorProperties(ParticleEffect effect, int effectAmount, ParticleEffect.OrdinaryColor oc) {
+
+	public AppliedDescriptorProperties(ParticleEffect effect, int effectAmount, float speed,
+			ParticleEffect.OrdinaryColor oc)
+	{
 		this.effect = effect;
 		this.effectAmount = effectAmount;
-		this.speed = 0f;
+		this.speed = speed;
 		this.oc = oc;
 		this.nc = null;
 	}
-	
-	public AppliedDescriptorProperties(ParticleEffect effect, int effectAmount, ParticleEffect.NoteColor nc) {
+
+	public AppliedDescriptorProperties(ParticleEffect effect, int effectAmount, float speed,
+			ParticleEffect.NoteColor nc)
+	{
 		this.effect = effect;
 		this.effectAmount = effectAmount;
-		this.speed = 0f;
+		this.speed = speed;
 		this.oc = null;
 		this.nc = nc;
 	}
-	
+
 	public ParticleEffect getEffect()
 	{
 		return this.effect;
 	}
-	
+
 	public int getAmount()
 	{
 		return this.effectAmount;
@@ -62,7 +70,5 @@ public class AppliedDescriptorProperties {
 	{
 		return speed;
 	}
-	
-	
-	
+
 }
