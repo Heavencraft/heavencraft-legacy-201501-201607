c3c0c8051937438af6036391c372379203b4d2eb
Manuel R <rauscher.manuel@gmail.com>
2016-04-10 22:52:14 +0200

    [RP] Fixed PVP & PVP4 not clearing potion effects. Silly me, was trying to clear a unreferenced collection.

--- heavenrp/src/main/java/fr/lorgan17/heavenrp/listeners/PVPManager.java
+++ heavenrp/src/main/java/fr/lorgan17/heavenrp/listeners/PVPManager.java
@@ -250,7 +250,8 @@ public class PVPManager extends AbstractListener<HeavenPlugin>
 				p.getInventory().setChestplate(null);
 				p.getInventory().setHelmet(null);
 
-				p.getActivePotionEffects().clear();
+				for (PotionEffect effect : p.getActivePotionEffects())
+					p.removePotionEffect(effect.getType());
 
 				p.setHealth(20);
 				p.setFoodLevel(30);
