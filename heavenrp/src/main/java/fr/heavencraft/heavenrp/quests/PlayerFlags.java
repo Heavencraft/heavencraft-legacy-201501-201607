aa7f9b79ecf0b568ff78da9f359dfa35f738a750
Manuel R <rauscher.manuel@gmail.com>
2016-06-08 18:42:06 +0200

    [SRP] Quests should use the abstract class, so we no longer need to use the interface, and also cleaned up some code.

---
+++ heavenrp/src/main/java/fr/heavencraft/heavenrp/quests/PlayerFlags.java
@@ -0,0 +1,27 @@
+package fr.heavencraft.heavenrp.quests;
+
+import fr.heavencraft.heavenrp.questframework.QfFlag;
+
+/**
+ * This enum should contain all player flags, which are shared across all quests
+ * @author Manuel
+ *
+ */
+public enum PlayerFlags
+{
+	// Flag to show usage.
+	FIRST_TEST_FLAG(new QfFlag("pFirstTestFlag")),
+	SECOND_TEST_FLAG(new QfFlag("pSecondTestFlag"));
+	
+	private final QfFlag flag;
+
+	private PlayerFlags(QfFlag flag)
+	{
+		this.flag = flag;
+	}
+
+	public QfFlag getFlag()
+	{
+		return flag;
+	}	
+}
