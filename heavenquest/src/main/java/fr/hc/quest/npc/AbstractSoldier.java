7b11da1a748533e54bb0598d0984141e25982e4f
LRGN <lrgn@protonmail.com>
2016-07-13 18:13:02 +0900

    Working at work

--- heavenquest/src/main/java/fr/hc/quest/npc/AbstractSoldier.java
+++ heavenquest/src/main/java/fr/hc/quest/npc/AbstractSoldier.java
@@ -2,18 +2,27 @@ package fr.hc.quest.npc;
 
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
 
 public abstract class AbstractSoldier implements Soldier
 {
-	private final NPC npc;
-	private final Location spawnLocation;
+	protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	public AbstractSoldier(String name, Location spawnLocation)
+	private final String name;
+	private final Location spawnLocation;
+	private final Team team;
+
+	protected final NPC npc;
+
+	public AbstractSoldier(String name, Location spawnLocation, Team team)
 	{
+		this.name = name;
 		this.spawnLocation = spawnLocation;
+		this.team = team;
 
 		npc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, name);
 		npc.setProtected(false);
@@ -25,6 +34,7 @@ public abstract class AbstractSoldier implements Soldier
 	@Override
 	public void onDeath()
 	{
+		log.info("Soldier {} is dead, respawning NPC {}.", name, npc);
 		npc.spawn(spawnLocation);
 	}
 }
--- heavenquest/src/main/java/fr/hc/quest/npc/DefenseSoldier.java
+++ heavenquest/src/main/java/fr/hc/quest/npc/DefenseSoldier.java
@@ -1,33 +1,24 @@
 package fr.hc.quest.npc;
 
 import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
 
 import fr.hc.quest.TargetNearbyNPCBehavior;
 import fr.hc.quest.strategy.DefenseStrategy;
-import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.ai.goals.MoveToGoal;
-import net.citizensnpcs.api.npc.NPC;
 
 /*
  * NPC that move to a point and defend it until it dies, then respawn
  */
-public class DefenseSoldier
+public class DefenseSoldier extends AbstractSoldier
 {
-	private final NPC npc;
-
 	public DefenseSoldier(Location spawnLocation, Location defenseLocation)
 	{
-		npc = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, "Soldier");
-		npc.data().set("team", "citadel");
+		super("DefenseSoldier", spawnLocation, Team.CITADEL);
 
 		npc.getNavigator().getLocalParameters().attackRange(25);
 		npc.getNavigator().getLocalParameters().attackStrategy(new DefenseStrategy());
 
 		npc.getDefaultGoalController().addBehavior(new MoveToGoal(npc, defenseLocation), 1);
 		npc.getDefaultGoalController().addBehavior(new TargetNearbyNPCBehavior(npc), 2);
-
-		npc.spawn(spawnLocation);
-		npc.setProtected(false);
 	}
 }
